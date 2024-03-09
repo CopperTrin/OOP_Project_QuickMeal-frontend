72fd839185bce9d4d4c07df8a24aa303301e848b
BigdraCo1 <pakawat.tab@gmail.com>
2024-03-09 13:05:26 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,99 @@
+import { useEffect, useState } from "react";
+import axios from 'axios';
+import './App.css'
+import starLogo from './assets/star.svg'
+import lensLogo from './assets/lens.svg'
+
+const BASE_URL = 'http://127.0.0.1:8000';
+
+export function App() {
+  const [key, setKey] = useState('');
+  const [todos, setTodos] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function fetchTodo(key) {
+    try {
+      setIsLoading(true);
+      let response;
+      if (key === '') {
+        response = await axios.get(`${BASE_URL}/show/restaurant`);
+      } else {
+        response = await axios.get(`${BASE_URL}/search/${key}`);
+      }
+      if (response.status === 200){
+      setTodos(response.data);
+      }
+      
+
+    } catch (error) {
+      console.log('error', error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() =>{
+    fetchTodo(key)
+  },[])
+
+
+  const handleChange = (event) => {
+    setKey(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    fetchTodo(key);
+  };
+
+  return (
+    <>
+      <section className="grid place-items-center bg-emerald-900 p-16 min-h-screen">
+        <div>
+          <div className="flex gap-2 relative  shadow-md sm:rounded-lg">
+            <div class="relative mt-0">
+            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <img src={lensLogo} className="logo" alt="Star logo" />
+            </div>
+            <input
+              className="mt-1   px-3 py-2  border border-slate-1000  shadow-sm bg-gray-50 focus:outline-none focus:border-green-200 text-gray-900 text-sm rounded-lg  block w-80 pl-10 p-2.5   dark:border-gray-600 dark:placeholder-gray-400 dark:text-black "
+              type="text"
+              placeholder="สั่งอะไรดี?"
+              value={key}
+              onChange={handleChange}
+              onKeyDown={(e) => {
+                  if (e.key === 'Enter') {
+                  handleSubmit();
+              }
+               }}
+            />
+            </div>
+            <button
+              className="h-12 min-w-[8rem] rounded-lg border-2 border-emerald-600 bg-emerald-500 text-emerald-50 shadow-lg hover:bg-emerald-600 focus:outline-none focus:ring focus:ring-emerald-600"
+              onClick={handleSubmit}
+            >
+              Submit
+            </button>
+          </div>
+          <br />
+
+          {isLoading ? (
+            <div>Loading..</div>
+          ) : (
+            todos.map((todo, index) => (
+              <div key={index} className="border border-gray-200 p-4 mb-4 rounded-md bg-white flex flex-col justify-center items-center">
+            <div className="font-semibold">{todo._Restaurant__name_restaurant}</div>
+            <div className="flex items-center ">
+            <img src={starLogo} className="logo" alt="Star logo" />
+            <div className="font-semibold">{todo._Restaurant__rate}</div>
+            </div>
+            </div>
+            ))
+          )}
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default App;
+
---
+++ src/Menu.jsx
@@ -0,0 +1,79 @@
+import { useState, useEffect } from 'react'
+import { useParams, Link } from 'react-router-dom'
+import axious from 'axios'
+
+const BASE_URL = 'http://127.0.0.1:8000'
+
+function Menu(){
+    const {restaurant} = useParams()
+    const [menues, setMenues] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    
+    async function fetchMenu(restaurant){
+        try{
+            setIsLoading(true);
+            const response = await axious.get(`${BASE_URL}/${restaurant}`)
+            setMenues(response.data)
+        }
+        catch (error){
+            console.log('error' , error)
+        }
+        finally {
+            setIsLoading(false);
+        }
+    }
+
+    async function deleteMenu(name_menu){
+        try{
+            setIsLoading(true)
+            await axious.delete(`${BASE_URL}/${restaurant}/${name_menu}`)
+            await fetchMenu(restaurant)
+        }catch (error){
+            console.log('error' , error)
+        }
+        finally {
+            setIsLoading(false);
+        }
+    }
+
+    useEffect( () =>{
+    fetchMenu(restaurant)}
+    ,[])
+
+    return (
+        <>
+        <section className="grid place-items-center bg-emerald-900 p-16 min-h-screen">
+            {isLoading && (<p className="border border-gray-200 p-10 rounded-md bg-white flex flex-col justify-center items-centertext-2xl font-bold mb-1">Loading..</p>)}
+                { !isLoading && <div>
+                    {
+                        menues.map((menu, index) => (
+                        <div key={index} className="border border-gray-200 p-10 mb-4 rounded-md bg-white flex flex-col justify-center items-center">
+                            <h2 className="text-2xl font-bold mb-2">{menu._Food__name}</h2>
+                            <p className="font-bold mb-1">
+                            Type : {menu._Food__type} 
+                            </p> 
+                        <div>
+                    {Object.entries(menu._Food__size).map(([key, value]) => (
+                    <div className="table-row" key={key}>
+                        <div>{key} +{value}</div>
+                    </div>
+                ))}
+                </div>
+                <p className="font-bold mb-1">
+                    Price : {menu._Food__price} 
+                </p>
+                <button class="bg-green-600 text-white px-4 py-2 rounded mb-2">Edit</button>
+                <button class="bg-red-600 text-white px-4 py-2 rounded"
+                onClick={async () => {
+                    await deleteMenu(menu._Food__name)
+                }}
+                >Delete</button>
+                </div>
+                ))}
+                </div>}
+        </section>
+        </>
+  )
+}
+
+export default Menu
